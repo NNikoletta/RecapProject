1f4df00df2c99ae548f281ab5fb7153dde515991
Nikoletta <nikolettanod@gmail.com>
2026-06-27 00:21:12 -0400

    Beginner NN - XOR function practice

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-print("Finally works")
-print("Second try")
-print("Third try")
-print("Fourth try")
-print("Example")
+import torch
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.optimizers import SGD
+
+
+np.random.seed(444)
+
+X = np.array([
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]])
+
+y = np.array([[0], [1], [1], [0]])
+
+model = Sequential()
+model.add(Dense(2, input_dim=2))
+model.add(Activation('sigmoid'))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+sgd = SGD(learning_rate=0.1)
+model.compile(loss='MeanSquaredError', optimizer=sgd)
+
+model.fit(X, y, batch_size=1, epochs=5000)
+
+if __name__ == '__main__':
+    print(model.predict(X))
