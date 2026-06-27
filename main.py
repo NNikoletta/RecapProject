516c08ad823552a18b0fe4f96a16391e4c6f9045
Nikoletta <nikolettanod@gmail.com>
2026-06-27 01:01:15 -0400

    Beginner Picture classification - CIFAR10 dataset (1)

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
+import matplotlib.pyplot as plt
 import torch
+import torchvision
+import torchvision.transforms as transforms
 import numpy as np
 
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-from keras.optimizers import SGD
+batch_size = 10
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
 
+print("Downloading training data...")
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
-np.random.seed(444)
+print("Downloading testing data...")
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
-X = np.array([
-    [0, 0],
-    [0, 1],
-    [1, 0],
-    [1, 1]])
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-y = np.array([[0], [1], [1], [0]])
+dataiter =iter(trainloader)
+images, labels = dataiter.next()
 
-model = Sequential()
-model.add(Dense(2, input_dim=2))
-model.add(Activation('sigmoid'))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+for i in range(batch_size):
+    plt.subplot(2, int(batch_size/2), i + 1)
+    img = images[i]
+    img = img/2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.axis('off')
+    plt.title(classes[labels[i]])
 
-sgd = SGD(learning_rate=0.1)
-model.compile(loss='MeanSquaredError', optimizer=sgd)
+plt.subtitle('Preview of Training Data', size=20)
+plt.show()
 
-model.fit(X, y, batch_size=1, epochs=5000)
-
-if __name__ == '__main__':
-    print(model.predict(X))
